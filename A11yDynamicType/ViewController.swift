66111d6cb0142f6aea17cbcf300f65e7cb022fe0
Winnie Teichmann <4530+thatswinnie@users.noreply.github.com>
2022-07-28 08:53:27 -0400

    Initial Commit

---
+++ A11yDynamicType/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A11yDynamicType
+//
+//  Created by Winnie Teichmann on 2022-07-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
